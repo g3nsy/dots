de22b1d544338d8ba34e9f42d5df78a5a752f592
g3nsy <g3nsydev@gmail.com>
2024-08-16 09:43:39 +0200

    Removed nvim dir

--- configs/nvim/lua/plugins/disabled.lua
+++
@@ -1,8 +0,0 @@
-return {
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
-  { "nvim-lualine/lualine.nvim", enabled = false },
-  { "catppuccin/nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
-  -- { "hrsh7th/nvim-cmp", enabled = false },
-  { "Bilal2453/luvit-meta", enabled = false },
-}
